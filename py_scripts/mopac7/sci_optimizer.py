f617c05261ea730cc813103fcab3976cf6c05c50
Sebastijonas Valaitis <d.dizya@gmail.com>
2025-10-20 11:27:23 +0300

    Update

--- py_scripts/mopac7/sci_optimizer.py
+++ py_scripts/mopac7/sci_optimizer.py
@@ -4,22 +4,18 @@ import subprocess, os, time
 from datetime import datetime
 from scipy.optimize import  minimize
 
-KCAL_TO_KJ = 4.184
-EV_TO_KJMOL = 96.4853321233
-EV_TO_KCALMOL = 23.060548
-
 atoms_list = [""]
 
 MOPAC7_PATH = "C:/Users/ddizy/Desktop/mopac7/"
 RESULT_PATH = "C:/Users/ddizy/Desktop/qcproject/py_scripts/mopac7/mopac7_results/"
-NAME_XYZ = "ch3cl_for_grad_test.xyz"
+NAME_XYZ = "TS_FCH3Cl.xyz"
 NAME_XYZ_OPTIMIZED = NAME_XYZ.replace(".xyz", "_optimized.xyz")
 
 
 multiplicity =  ["INVALID_MULTIPLICITY", "SINGLET", "DOUBLET", "TRIPLET", "QUARTET", "QUINTET", "SEXTET"]
 PROJECT_NAME=NAME_XYZ
 COMMENT="DFT_Energy_Gradient"
-CHARGE=0
+CHARGE=-1
 MULTI=multiplicity[1]
 
 #**********************************
@@ -55,6 +51,13 @@ def create_for005(atoms, geometry, timestamp):
     with open(MOPAC7_PATH + "FOR005", "w") as f:
         f.write(input_text)
 
+def wait_for_for006(timeout=120, poll=1):
+    t0 = time.time()
+    while not os.path.exists(MOPAC7_PATH + "FOR006"):
+        if time.time() - t0 > timeout:
+            raise TimeoutError("FOR 006 was not created")
+        time.sleep(poll)
+
 def clear_forxxx():
     for file in ("FOR006", "FOR009", "FOR011", "FOR012", "FOR016", "SHUTDOWN"):
         if os.path.exists(MOPAC7_PATH + file):
@@ -64,9 +67,7 @@ def run_mopac_exe():
     subprocess.run([str(MOPAC7_PATH + "mopac7.exe")], cwd=str(MOPAC7_PATH),text=True, check=False)
 
 def parse_for006():
-    if not os.path.exists(MOPAC7_PATH + "FOR006"):
-        time.sleep(11)
-        print("slept for a sec")
+    wait_for_for006()
 
     with open(MOPAC7_PATH + "FOR006") as f:
         text = f.read()
@@ -74,17 +75,12 @@ def parse_for006():
     readFrom = text.find("TOTAL ENERGY")
     readTo = text[readFrom:].find("EV")+readFrom
     total_energy_raw = text[readFrom:readTo]
-    total_energy = float(total_energy_raw.split()[-1])*EV_TO_KJMOL
+    total_energy = float(total_energy_raw.split()[-1])
 
     readFrom = text.find("CARTESIAN COORDINATE DERIVATIVES")
     gradients = np.array(text[readFrom:].split())[8:]
     index=np.where(gradients=="-------------------------------------------------------------------------------")[0][0]
     gradients = gradients[:index].reshape(-1, 5)
-
-    for row in gradients:
-        row[2] = float(row[2]) * KCAL_TO_KJ
-        row[3] = float(row[3]) * KCAL_TO_KJ
-        row[4] = float(row[4]) * KCAL_TO_KJ
 
     return total_energy, gradients
 
@@ -94,14 +90,13 @@ def create_report(total_energy, gradients, timestamp):
     with open(MOPAC7_PATH + "FOR005") as f:
         for006 = f.read()
     result =f"""{PROJECT_NAME}, {timestamp}, {COMMENT} \n
-    TOTAL ENERGY = {total_energy} KJ/MOL\n
-    CARTESIAN COORDINATE DERIVATIVES, kJ/mol/A
+    TOTAL ENERGY = {total_energy} EV\n
+    CARTESIAN COORDINATE DERIVATIVES, EV/A
     {pd.DataFrame(gradients)}\n
     ***FOR006 input***
     {for006}\n
     ***  XYZ input ***
     {xyz}"""
-    #print("DONE")
     with open(RESULT_PATH+"result"+timestamp+".txt", "w") as f:
         f.write(result)
 
@@ -111,8 +106,8 @@ def sci_optimize():
 
     def get_flat_geometry():
         _, geometry = import_xyz()
-        flat_geometry = geometry.reshape(-1, 4)[:, 1:].reshape(-1)
-        return flat_geometry.astype(float)
+        flat_geometry = geometry.reshape(-1, 4)[:, 1:].reshape(-1).astype(float)
+        return flat_geometry
 
     def rewrite_xyz(new_geometry_flat):
         with open(MOPAC7_PATH + NAME_XYZ) as f:
@@ -127,7 +122,6 @@ def sci_optimize():
             new_xyz += " ".join(str(x) for x in symbol) + f"\n"
         with open(MOPAC7_PATH + NAME_XYZ, "w") as f:
             f.write(new_xyz)
-        #print("XYZ FILE REWROTE")
 
     def get_gradient_and_energy_from_flat_geometry(x):
         rewrite_xyz(x)
@@ -136,11 +130,18 @@ def sci_optimize():
 
         return float(total_energy), gradient.reshape(-1).astype(float)
 
-    res = minimize(fun = get_gradient_and_energy_from_flat_geometry,
-                   x0=get_flat_geometry(),
-                   jac = True,
-                   method="L-BFGS-B",
-                   )
-    print(res)
+    end_result = minimize(
+        fun = get_gradient_and_energy_from_flat_geometry,
+        x0=get_flat_geometry(),
+        jac = True,
+        method="L-BFGS-B",
+        options = dict(
+            maxiter=300,
+            gtol=1e-5,
+            ftol=0.0,
+            maxls=50
+        ))
+    print(end_result)
+
 
 sci_optimize()
